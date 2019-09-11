be0953181de176bca1a3e33412b0ed5570ee06e7
Stathis Stamoulis <sstamoulis2010@gmail.com>
2019-09-11 15:04:13 +0000

    Add http server

--- components/wifi_connect/wifi_connect.c
+++ components/wifi_connect/wifi_connect.c
@@ -1,5 +1,6 @@
 #include "wifi_connect.h"
 #include <esp_event_loop.h>
+#include <esp_http_server.h>
 #include <esp_log.h>
 #include <esp_wifi.h>
 #include <freertos/event_groups.h>
@@ -43,32 +44,52 @@ esp_err_t event_handler(void *ctx, system_event_t *event) {
     return ESP_OK;
 }
 
-char *get_ap_ssid() {
-    const static char *AP_SSID_FORMAT = "Nile_%02X%02X%02X%02X%02X%02X";
-    char ssid[32];
-    char mac[6];
-    ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_AP, &mac));
-    sprintf(ssid, AP_SSID_FORMAT, mac);
-    return ssid;
+void get_ap_ssid(char *ssid) {
+    const static char AP_SSID_FORMAT[] = "Nile_%02X%02X%02X";
+    uint8_t mac[6];
+    ESP_ERROR_CHECK(esp_wifi_get_mac(ESP_IF_WIFI_AP, mac));
+    snprintf(ssid, sizeof(((wifi_config_t *)0)->ap.ssid) + 1, AP_SSID_FORMAT,
+             mac[3], mac[4], mac[5]);
 }
 
+static esp_err_t http_get_handler(httpd_req_t *req) {
+    httpd_resp_send(req, "Hello World!", -1);
+    return ESP_OK;
+}
+
+static const httpd_uri_t hello = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = http_get_handler,
+    .user_ctx = NULL,
+};
+
 void switch_to_wifi_configuration_mode() {
-    wifi_config_t wifi_config = (wifi_config_t){
+    char ssid[sizeof(((wifi_config_t *)0)->ap.ssid) + 1];
+    get_ap_ssid(ssid);
+    // wifi_config_t wifi_config = {0};
+    wifi_config_t wifi_config = {
         .ap =
-            (wifi_ap_config_t){
-                .ssid = get_ap_ssid(),
+            {
                 .password = "nileisariver",
+                .max_connection = 4,
                 .authmode = WIFI_AUTH_WPA_WPA2_PSK,
             },
     };
-    ESP_ERROR_CHECK(esp_wifi_stop());
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+    strncpy((char *)wifi_config.ap.ssid, ssid,
+            sizeof(((wifi_config_t *)0)->ap.ssid));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
     ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "Started AP: ssid=\"%s\", pass=\"%s\"", wifi_config.ap.ssid,
+             wifi_config.ap.password);
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    ESP_ERROR_CHECK(httpd_start(&server, &config));
+    httpd_register_uri_handler(server, &hello);
 }
 
 void switch_to_wifi_connection_mode() {
-    ESP_ERROR_CHECK(esp_wifi_stop());
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
@@ -86,7 +107,7 @@ void wifi_init() {
     ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_LOGI(TAG, "Stored WiFi: ssid=%s, pass=%s", wifi_config.sta.ssid,
              wifi_config.sta.password);
-    if (wifi_config.sta.ssid[0] == NULL) {
+    if (wifi_config.sta.ssid[0] == 0) {
         // No station configuration is stored on chip
         // Start WiFi configuration task
         switch_to_wifi_configuration_mode();
